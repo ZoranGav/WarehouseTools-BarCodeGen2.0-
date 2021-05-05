4877ae33da9d6c721eb66d9179d6a90ca236787a
ZoranGav <52153970+ZoranGav@users.noreply.github.com>
2021-05-05 11:48:24 +0200

    WarehouseTools-BarCodeGen2.0(beta)

---
+++ BarCode/js/lpn.js
@@ -0,0 +1,50 @@
+window.addEventListener('load', () => {
+    var params = (new URL(document.location)).searchParams;
+    
+    /*This .js file and web page are disabled*/
+
+    /* Parametars from lpn.html*/
+    var lpnnumber = params.get('lpnnumber');
+    var container = document.getElementById("container");
+
+    if ("last" in localStorage) {
+        
+        numberOfLotCodes(lpnnumber);
+
+    }
+    else {
+
+        localStorage.setItem('last', 10000001);
+        numberOfLotCodes(lpnnumber);
+
+    }
+
+    /* Formating LPNs */
+    function myLpn(a){
+        let myString = '90000000';
+        let myString2 = myString.slice(0, -a.length) + a;
+            return myString2;
+    
+    }
+
+    /* Generating LPNs */
+    function numberOfLotCodes(lpnFromUser){
+        let lpn = localStorage.getItem('last');
+        for (let i = 0; i < lpnFromUser; i++) {
+            lpn++;
+            var ba = 'BA' + (lpn - 1);
+            console.log(ba);
+            container.innerHTML += `<svg id = ${ba} ></svg>`;
+            var ba = JsBarcode(`#${ba}`, ba, { format: "CODE128A", width: 3, height: 150 });
+        }
+        localStorage.removeItem('last');
+        localStorage.setItem('last', lpn);
+        console.log(lpn);
+    }
+
+
+
+
+
+
+})
---
+++ BarCode/js/lpnLCNO.js
@@ -0,0 +1,40 @@
+window.addEventListener('load', () => {
+    var params = (new URL(document.location)).searchParams;
+    
+    
+/* Parametars from lpnLCBO.html*/
+    var lpnnumber = params.get('lpnnumber');
+    var lpnFrom = params.get('lpnfrom');
+    var container = document.getElementById("container");
+    
+/* Formating LPNs */
+    function myLpn(a){
+        let myString = '90000000';
+        let myString2 = myString.slice(0, -a.length) + a;
+            return myString2;
+    
+    }
+
+/* Generating LPNs */
+    function numberOfLotCodes(lpnFromUser, lpnFrom){
+        let lpn = myLpn(lpnFrom);
+        
+        for (let i = 0; i < lpnFromUser; i++) {
+            lpn++;
+            var lpnPrefix = 'BA' + (lpn - 1);
+            console.log(lpnPrefix);
+            container.innerHTML += `<svg id = ${lpnPrefix} ></svg>`;
+            var ba = JsBarcode(`#${lpnPrefix}`, lpnPrefix, { format: "CODE128A", width: 3, height: 150 });
+        }
+    }
+
+    if (lpnFrom != 0) {
+        
+        numberOfLotCodes(lpnnumber, lpnFrom);
+        console.log(lpnFrom);
+
+    }
+
+
+
+})
